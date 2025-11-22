5847f3b58efc65e221defd359100935536bda3cd
Muqtada Al-Saedi <76435059+royspotsec@users.noreply.github.com>
2025-11-22 22:08:00 +0300

    so new futures

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT 
+from player import *
 
 
     
@@ -11,17 +12,31 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    dt = 0
+    clock = pygame.time.Clock()
+    
+    
+    
     while(True):
         log_state()
+        
         for event in pygame.event.get():
+            dt = clock.tick(60)
+            
         
             if event.type == pygame.QUIT:
                 return 
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        
+    
+        
     
 
 
